448df2b45bdf88a21c089aef406be1af4992fd26
Dton04 <tandat081104@gmail.com>
2025-05-28 10:02:55 +0700

    2280600609 Nguyen Tan Dat

--- ContactList/ContactList/AddContactForm.Designer.cs
+++ ContactList/ContactList/AddContactForm.Designer.cs
@@ -33,14 +33,19 @@
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+<<<<<<< HEAD
             this.textBox2 = new System.Windows.Forms.TextBox();
+=======
+            this.Email = new System.Windows.Forms.TextBox();
+>>>>>>> ebabe5ef879bb3b5ef3f68a646fb70f2fab3cc37
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(164, 95);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(92, 141);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(100, 27);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Ho Ten";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
@@ -53,21 +58,28 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(301, 95);
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(264, 141);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.Size = new System.Drawing.Size(138, 27);
             this.textBox3.TabIndex = 3;
             this.textBox3.Text = "So Dien Thoai";
             // 
             // button1
             // 
+<<<<<<< HEAD
             this.button1.Location = new System.Drawing.Point(615, 95);
+=======
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(658, 141);
+>>>>>>> ebabe5ef879bb3b5ef3f68a646fb70f2fab3cc37
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(95, 28);
             this.button1.TabIndex = 5;
-            this.button1.Text = "Luu";
+            this.button1.Text = "Lưu";
             this.button1.UseVisualStyleBackColor = true;
             // 
+<<<<<<< HEAD
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -76,13 +88,27 @@
             this.textBox2.Size = new System.Drawing.Size(100, 27);
             this.textBox2.TabIndex = 6;
             this.textBox2.Text = "Email";
+=======
+            // Email
+            // 
+            this.Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Email.Location = new System.Drawing.Point(468, 141);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(100, 27);
+            this.Email.TabIndex = 6;
+            this.Email.Text = "Email";
+>>>>>>> ebabe5ef879bb3b5ef3f68a646fb70f2fab3cc37
             // 
             // AddContactForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+<<<<<<< HEAD
             this.Controls.Add(this.textBox2);
+=======
+            this.Controls.Add(this.Email);
+>>>>>>> ebabe5ef879bb3b5ef3f68a646fb70f2fab3cc37
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox1);
@@ -99,6 +125,10 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button1;
+<<<<<<< HEAD
         private System.Windows.Forms.TextBox textBox2;
+=======
+        private System.Windows.Forms.TextBox Email;
+>>>>>>> ebabe5ef879bb3b5ef3f68a646fb70f2fab3cc37
     }
 }
--- ContactList/ContactList/Form1.Designer.cs
+++ ContactList/ContactList/Form1.Designer.cs
@@ -57,10 +57,11 @@
             // 
             this.button1.Location = new System.Drawing.Point(359, 251);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(121, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Them lien he";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainForm
             // 
