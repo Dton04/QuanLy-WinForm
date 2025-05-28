88e295746f0dc2f33bfacbcd1f3ee1e5b1791e77
ToiFriendly <trantantoirok@gmail.com>
2025-05-28 09:09:44 +0700

    2280603320_TranTanToi Them Lien He Moi

--- ContactList/ContactList/Form1.Designer.cs
+++ ContactList/ContactList/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.btnThemlienhe = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -52,11 +53,21 @@
             this.btnThemlienhe.Text = "Thêm liên hệ";
             this.btnThemlienhe.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(359, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Them lien he";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnThemlienhe);
             this.Controls.Add(this.label1);
             this.Name = "MainForm";
@@ -70,6 +81,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnThemlienhe;
+        private System.Windows.Forms.Button button1;
     }
 }
 
